4e2bb8e08dce178f66a16cfd52ae16aebbae7c69
Cort Harlow <cort.harlow@gmail.com>
2016-01-20 18:17:37 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+const express     = require('express');
+const logger      = require('morgan');
+const path        = require('path');
+const request     = require('request');
+const bodyParser  = require('body-parser');
+const jwt         = require('jsonwebtoken');
+const app         = express();
+
+let server = require('http').createServer(app);
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/moodtrackr')
+
+server.listen(process.env.PORT || 3000, function() {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
